eb019659d904ddff620ad81f09c145a0dd76ea8d
Walker Trent <walktrent@gmail.com>
2018-10-08 23:41:29 +0000

    Added form validation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,9 +6,11 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
+      flash[:success] = "Message sent."
       redirect_to new_contact_path, notice: "Message sent."
-    else 
-      redirect_to new_contact_path, notice: "Error occured."
+    else
+      flash[:error] = @contact.errors.full_messages.join(", ")
+      redirect_to new_contact_path
     end
   end
   
