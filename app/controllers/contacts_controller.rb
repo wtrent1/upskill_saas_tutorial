b99d05e52f9edb17e0609d2ee92998659f2531b5
Walker Trent <walktrent@gmail.com>
2018-10-09 00:03:20 +0000

    Improved form validation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,9 +7,9 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     if @contact.save
       flash[:success] = "Message sent."
-      redirect_to new_contact_path, notice: "Message sent."
+      redirect_to new_contact_path
     else
-      flash[:error] = @contact.errors.full_messages.join(", ")
+      flash[:danger] = @contact.errors.full_messages.join(", ")
       redirect_to new_contact_path
     end
   end
